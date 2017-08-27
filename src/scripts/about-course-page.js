9ae38682f5ba12e3912f5243cf16348aea92d864
Rostyslav <rostyslav4yk@gmail.com>
2017-08-27 19:10:34 +0300

    courese tablet-mobile almost

--- src/scripts/about-course-page.js
+++ src/scripts/about-course-page.js
@@ -127,57 +127,10 @@ const aboutCoursePage = () => {
 
     });
     $('.cr-courses-slider').slick({
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       arrows: true,
       dots: false,
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            centerMode: true,
-            centerPadding: '150px',
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            centerMode: true,
-            centerPadding: '150px',
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true
-          }
-        },
-         {
-          breakpoint: 692,
-          settings: {
-            centerMode: true,
-            centerPadding: '200px',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            centerMode: true,
-            centerPadding: '10px',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true
-          }
-        }
-      ]
-
     });
   });
 
