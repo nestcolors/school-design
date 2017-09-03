a7aa4f9329afbaedb8421c69929e7cfd65768765
Severynsky <severynr@gmail.com>
2017-09-03 18:23:37 +0300

    spaces switcher, not final

--- src/scripts/spaces.js
+++ src/scripts/spaces.js
@@ -2,21 +2,42 @@ import $ from "jquery";
 const spaces = () => {
   console.log('spaces');
 
-  $(document).ready(function(){
-      $('.cr-spaces-slider').slick({
-        arrows: true,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              dots: true
-            }
-          }
-        ]
-      });
+  $('.space-selector-item').click(function(e) {
+    e.preventDefault()
+    const nextSpace = e.target.getAttribute('data-space-name');
+    const currentSlider = $('.visible-slider');
+    const currentLink = $('.active-link');
 
+    // handling active state for spaces link
+    currentLink.removeClass('active-link');
+    $(`.${nextSpace}`).addClass('active-link');
+
+    // handling active state for spaces gallery
+    currentSlider.removeClass('visible-slider');
+    $(`#${nextSpace}`).addClass('visible-slider');
+
+    initSlick(nextSpace);
+    $(currentSlider[0]).slick('unslick');
+  })
+
+  $(document).ready(function(){
+    initSlick($('.cr-spaces-slider')[0].id);
   });
+
+  const initSlick = (val) => {
+    $(`#${val}`).slick({
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            dots: true
+          }
+        }
+      ]
+    });
+  }
 
 }
 
